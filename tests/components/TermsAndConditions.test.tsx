0ade929024689a5e1e7569fc7f90552f6a6d7b83
Prashant love coding <dawkharp79@gmail.com>
2025-10-02 21:08:43 +0530

    Added common function to get the DOM elements.

--- tests/components/TermsAndConditions.test.tsx
+++ tests/components/TermsAndConditions.test.tsx
@@ -3,30 +3,29 @@ import TermsAndConditions from "../../src/components/TermsAndConditions";
 import userEvent from "@testing-library/user-event";
 
 describe("Terms and condition", () => {
-  it("should render content on UI", () => {
+  const renderComponent = () => {
     render(<TermsAndConditions />);
-    const headingElement = screen.getByRole("heading");
-    expect(headingElement).toBeInTheDocument();
+
+    return {
+      headingElement: screen.getByRole("heading"),
+      checkboxElement: screen.getByRole("checkbox"),
+      buttonElement: screen.getByRole("button"),
+    };
+  };
+
+  it("should render content on UI", () => {
+    const { headingElement, checkboxElement, buttonElement } =
+      renderComponent();
     expect(headingElement).toHaveTextContent(/Terms & Conditions/i);
-
-    const checkboxElement = screen.getByRole("checkbox");
-    expect(checkboxElement).toBeInTheDocument();
     expect(checkboxElement).not.toBeChecked();
-
-    const buttonElement = screen.getByRole("button");
-    expect(buttonElement).toBeInTheDocument();
     expect(buttonElement).toHaveTextContent(/submit/i);
     expect(buttonElement).toBeDisabled();
   });
 
   it("should enable the button when checkbox is checked", async () => {
-    render(<TermsAndConditions />);
-    const checkboxElement = screen.getByRole("checkbox");
+    const { checkboxElement, buttonElement } = renderComponent();
     const user = userEvent.setup();
     await user.click(checkboxElement);
-
-    const buttonElement = screen.getByRole("button");
-    expect(buttonElement).toBeInTheDocument();
     expect(buttonElement).toHaveTextContent(/submit/i);
     expect(buttonElement).toBeEnabled();
   });
