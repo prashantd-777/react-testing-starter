76c7ead3edc90754ef0a7b6b28401fad1d5bd844
Prashant love coding <dawkharp79@gmail.com>
2025-10-05 16:20:18 +0530

    Code changes

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,10 +1,7 @@
-import ExpandableText from "../components/ExpandableText";
-import Onboarding from "../components/Onboarding";
-import TermsAndConditions from "../components/TermsAndConditions";
+import SearchBox from "../components/SearchBox";
 
 const PlaygroundPage = () => {
-  return <ExpandableText text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque mollitia architecto, repellendus quos odio ducimus quaerat! Eveniet nesciunt quaerat rerum quasi error earum dolorum ullam, sed voluptas? Ducimus doloribus provident voluptatibus ab, pariatur alias possimus esse soluta unde blanditiis eligendi quidem optio aut. Ipsa consequuntur quam at esse. Esse reiciendis, dicta illo animi at consectetur repellat ipsa. Vel excepturi sint accusamus natus inventore quaerat, qui animi modi repellendus eos, perferendis eligendi. Autem tempora beatae sapiente esse, harum aspernatur sunt accusamus omnis error, porro in provident architecto culpa praesentium repudiandae assumenda nostrum enim nam tenetur! Ratione repellendus odio reprehenderit vitae. Nostrum.
-" />
+  return <SearchBox onChange={(text) => console.log(text)} />
 };
 
 export default PlaygroundPage;
---
+++ tests/components/SearchBox.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from "@testing-library/react";
+import SearchBox from "../../src/components/SearchBox";
+import userEvent from "@testing-library/user-event";
+import { vi } from "vitest";
+
+describe("SearchBox", () => {
+  const renderSearchbox = () => {
+    const onChange = vi.fn();
+    render(<SearchBox onChange={vi.fn()} />);
+    return {
+      inputElement: screen.getByPlaceholderText(/search/i),
+      onChange: onChange,
+      user: userEvent.setup(),
+    };
+  };
+
+  it("should render an input field for searching", () => {
+    const { inputElement } = renderSearchbox();
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  it.skip("should call onChange when Enter is pressed", async () => {
+    const { inputElement, onChange } = renderSearchbox();
+    const user = userEvent.setup();
+    const searchTerm = "SearchTerm";
+
+    await user.type(inputElement, searchTerm);
+
+    // wait for React state update
+    await new Promise((r) => setTimeout(r, 100));
+
+    // Press Enter
+    await user.keyboard("{Enter}");
+
+    expect(onChange).toHaveBeenCalledTimes(1);
+    expect(onChange).toHaveBeenCalledWith(searchTerm);
+  });
+
+  it("should not call onChanges if input field is empty", async () => {
+    const { inputElement, onChange, user } = renderSearchbox();
+    await user.type(inputElement, "{enter}");
+    expect(onChange).not.toHaveBeenCalled();
+  });
+});
